5cff1ed87443dedeef494d546124cb9f7ae194a6
Jonas Lopes <jonas.alp@gmail.com>
2021-02-01 20:32:06 -0300

    Primeiro Commit - Projeto criado

---
+++ src/main/java/br/com/jonaslopes/maisvendas/MaisvendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonaslopes.maisvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jonaslopes/maisvendas/MaisvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonaslopes.maisvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
