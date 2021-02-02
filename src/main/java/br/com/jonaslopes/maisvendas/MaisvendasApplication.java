e5725eb03f3bfe6ce51f407471af2eedfe39142a
Jonas Lopes <jonas.alp@gmail.com>
2021-02-02 12:42:04 -0300

    Criando Operação de Instanciação

--- src/main/java/br/com/jonaslopes/maisvendas/MaisvendasApplication.java
+++ src/main/java/br/com/jonaslopes/maisvendas/MaisvendasApplication.java
@@ -1,13 +1,32 @@
 package br.com.jonaslopes.maisvendas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MaisvendasApplication {
+import br.com.jonaslopes.maisvendas.domain.Categoria;
+import br.com.jonaslopes.maisvendas.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class MaisvendasApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MaisvendasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
 	}
 
 }
