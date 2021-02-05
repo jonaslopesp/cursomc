2b532ab4a9e80c8753ed4dceb0ea4a0c2067c3c4
Jonas Lopes <jonas.alp@gmail.com>
2021-02-04 21:59:31 -0300

    Listando Categorias com DTO

--- src/main/java/br/com/jonaslopes/maisvendas/resources/CategoriaResource.java
+++ src/main/java/br/com/jonaslopes/maisvendas/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.jonaslopes.maisvendas.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.jonaslopes.maisvendas.domain.Categoria;
+import br.com.jonaslopes.maisvendas.dto.CategoriaDTO;
 import br.com.jonaslopes.maisvendas.services.CategoriaService;
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -47,5 +50,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) throws ObjectNotFoundException {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() throws ObjectNotFoundException {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
