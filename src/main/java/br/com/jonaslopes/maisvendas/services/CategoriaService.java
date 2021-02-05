31fa66302020147e2a9f76377fd7249f68fe1b1b
Jonas Lopes <jonas.alp@gmail.com>
2021-02-04 22:43:16 -0300

    Paginação com parametros adicionais

--- src/main/java/br/com/jonaslopes/maisvendas/services/CategoriaService.java
+++ src/main/java/br/com/jonaslopes/maisvendas/services/CategoriaService.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Sort;
 import br.com.jonaslopes.maisvendas.domain.Categoria;
 import br.com.jonaslopes.maisvendas.repositories.CategoriaRepository;
 import br.com.jonaslopes.maisvendas.services.expeptions.DataIntegrityException;
@@ -47,5 +50,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
